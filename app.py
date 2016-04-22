fe063f749c28e7f5de1709ca6f65f3047c60728c
lee <lsn648693338@gmail.com>
2016-04-22 17:04:07 +0800

    Wrong

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask import  render_template
+
+app = Flask(__name__)
+bootsrtap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
